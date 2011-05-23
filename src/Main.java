15b6d8193621db2cf2aa141adb4df3cf4c8df0f5
Justin <xdv90x@gmail.com>
2011-05-23 15:30:12 -0500

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+import net.dv90.starfury.Server;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // TODO: Configuration
+        new Server().run();
+    }
+}
---
+++ src/net/dv90/starfury/Server.java
@@ -0,0 +1,16 @@
+package net.dv90.starfury;
+
+public class Server implements Runnable
+{
+
+    public static void main(String[] args)
+    {
+
+    }
+
+    public void run()
+    {
+        
+    }
+
+}
