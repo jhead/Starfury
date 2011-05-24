a17238bdab2858aee2594fd63a8535091235519c
Richard Lang <Oslarathos@gmail.com>
2011-05-24 17:02:28 -0400

    Moved the application entry point, initial stages of console command support.
    Added a splash screen on start
    
    Change-Id: I96dfefa43d9bb2f8dea865772ed0ab087c337aa8

---
+++ src/net/dv90/starfury/Console.java
@@ -0,0 +1,67 @@
+package net.dv90.starfury;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+
+
+import net.dv90.starfury.net.Server;
+import net.dv90.starfury.config.ServerConfig;
+import net.dv90.starfury.logging.*;
+
+public class Console extends Thread
+{
+	public static void main(String[] args)
+    {
+    	getConsole().start();
+    }
+	
+	private static Console instance = new Console();
+	
+	public static Console getConsole() {
+		return instance;
+	}
+	
+	private BufferedReader in;
+	private Server server;
+	
+	private Console() {
+		printFileContents( new File( "server.splash" ) );
+		
+		System.setErr( Logger.getPrintStream() );
+		
+		in = new BufferedReader( new InputStreamReader( System.in ) );
+		
+		server = new Server( ServerConfig.load() );
+		new Thread( server ).start();
+	}
+	
+	public void printFileContents( File file ) {
+		if ( file == null || !file.exists() )
+			return;
+		try {
+			FileReader freader = new FileReader( file );
+			BufferedReader reader = new BufferedReader( freader );
+			
+			String line = null;
+			while ( ( line = reader.readLine() ) != null ) {
+				System.out.println( line );
+			}
+		} catch ( Exception e ) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void run() {
+		try {
+			String line = null;
+			while ( ( line = in.readLine() ) != null ) {
+				// TODO: Console actions
+			}
+		} catch ( Exception e ) {
+			e.printStackTrace();
+		}
+	}
+}
