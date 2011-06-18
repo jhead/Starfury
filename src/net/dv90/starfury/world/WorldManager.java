6e0b6ce82c4fc1258421b29096b2cacacc70a50c
DeathCradle <DeathCradle@live.com.au>
2011-06-18 23:14:07 +1000

    Fixed Config (Annoyed me), Now adds defaults. Started World Loading.

--- src/net/dv90/starfury/world/WorldManager.java
+++ src/net/dv90/starfury/world/WorldManager.java
@@ -1,13 +1,10 @@
 package net.dv90.starfury.world;
 
 import java.awt.Point;
+import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.lang.ref.SoftReference;
-import java.nio.ByteOrder;
-import net.dv90.starfury.inventory.ItemStack;
-import net.dv90.starfury.inventory.ItemType;
-
+import java.io.IOException;
 import net.dv90.starfury.logging.*;
 import net.dv90.starfury.world.Tile.Type;
 
@@ -19,7 +16,7 @@ public class WorldManager {
     		folder.mkdir();
     }
     
-    public static World load(String worldName) throws Exception
+    public static World load(String worldName, int ProtocolVer) throws Exception
     {
         worldName = worldName.trim();
         if( worldName == null || worldName.length() == 0 )
@@ -31,7 +28,7 @@ public class WorldManager {
             return WorldManager.generate(worldName);
         
         Logger.log(LogLevel.INFO, "Loaded world '" + worldName + "'.");
-        return loadWorldDebugging( worldName );
+        return loadWorldDebugging( worldName, ProtocolVer );
     }
 
     public static World generate(String worldName) throws Exception
@@ -89,7 +86,48 @@ public class WorldManager {
     	
     }
     
-    public static World loadWorldDebugging( String worldName ) {
+    public static World loadWorldDebugging(String worldName, int protocolVersion) throws IOException {
+    	DataInputStream dataStream = new DataInputStream(new FileInputStream("Worlds\\" + worldName + ".wld" ));
+    	int compatabilityProtocol = dataStream.readInt();
+    	if(compatabilityProtocol != protocolVersion) {
+    		System.out.println("Incompatible World File!");
+    		dataStream.close();
+    		return null;
+    	} else {
+    		System.out.println("Compatible World File!");
+    	}
+    	
+    	World world = new World(dataStream.readUTF());
+    	world.setId(dataStream.readInt());
+    	dataStream.readInt();
+    	dataStream.readInt();
+    	dataStream.readInt();
+    	dataStream.readInt();
+    	world.setHeight(dataStream.readInt());
+    	world.setWidth(dataStream.readInt());
+    	world.setSpawn(new Point(dataStream.readInt(), dataStream.readInt()));
+    	world.setDirtLayer(dataStream.readInt());
+    	world.setRockLayer(dataStream.readInt());
+    	dataStream.readDouble(); //Temp time
+    	dataStream.readBoolean(); //Temp Day time
+    	dataStream.readInt(); //Temp Mood Phase
+    	dataStream.readBoolean(); //Temp Blood Moon
+    	dataStream.readInt(); //Dungeon X
+    	dataStream.readInt(); //Dungeon Y
+    	dataStream.readBoolean(); //Downed Boss #1
+    	dataStream.readBoolean(); //Downed Boss #2
+    	dataStream.readBoolean(); //Downed Boss #3
+    	dataStream.readBoolean(); //Shadow Orb Smashed?
+    	dataStream.readBoolean(); //Spawn Meteor?
+    	dataStream.readByte(); //Shadow Orb Count
+    	dataStream.readInt(); //Invasion Delay
+    	dataStream.readInt(); //Invasion Size
+    	dataStream.readInt(); //Invasion Type
+    	dataStream.readDouble(); //Invasion X
+    	
+    	//Load Tiles
+    	//Settle Liquids
+    	
     	return null;
     }
 }
